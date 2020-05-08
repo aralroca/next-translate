5508acbe31f612e2c45e33d9d72198beaf8cf66f
Aral Roca Gomez <aral-rg@hotmail.com>
2020-05-08 18:59:02 +0200

    Change html lang attribute on change language (#155)
    * Change html lang attribute on change language
    
    * Refactor

--- src/Link.js
+++ src/Link.js
@@ -1,3 +1,4 @@
+import React, { Children } from 'react'
 import NextLink from 'next/link'
 import useTranslation from './useTranslation'
 import fixAs from './_helpers/fixAs'
@@ -6,6 +7,17 @@ import fixHref from './_helpers/fixHref'
 export default function Link({ children, href, as, lang, noLang, ...props }) {
   const current = useTranslation()
   const lng = lang || current.lang
+  const child = Children.only(
+    typeof children === 'string' ? <a>{children}</a> : children
+  )
+
+  function onClick(e) {
+    const el = document.querySelector('html')
+    if (el) el.lang = lng
+    if (typeof child.props.onClick === 'function') {
+      child.props.onClick(e)
+    }
+  }
 
   return (
     <NextLink
@@ -13,7 +25,7 @@ export default function Link({ children, href, as, lang, noLang, ...props }) {
       as={noLang ? as : fixAs(as, href, lng)}
       {...props}
     >
-      {children}
+      {React.cloneElement(child, { onClick })}
     </NextLink>
   )
 }
--- src/Router.js
+++ src/Router.js
@@ -3,12 +3,15 @@ import clientSideLang from './clientSideLang'
 import fixAs from './_helpers/fixAs'
 import fixHref from './_helpers/fixHref'
 
-const nav = ev => (a1, a2, a3 = {}) => {
+const nav = (ev) => (a1, a2, a3 = {}) => {
   const a1IsObj = typeof a1 === 'object'
   const url = a1IsObj ? a1.url : a1
   const as = a1IsObj ? a1.as : a2
   const options = a1IsObj ? a1.options : a3
   const lng = options.lang || clientSideLang()
+  const el = document.querySelector('html')
+
+  if (el) el.lang = lng
 
   return NextRouter[ev](fixHref(url, lng), fixAs(as, url, lng), options)
 }
