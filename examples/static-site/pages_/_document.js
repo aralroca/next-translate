d3bfb628ac689a4a8faea0068ac2f3ec76942b49
Aral Roca Gomez <aral-rg@hotmail.com>
2020-05-26 12:41:28 +0200

    Add documentLang helper (#181)

--- examples/static-site/pages_/_document.js
+++ examples/static-site/pages_/_document.js
@@ -1,13 +1,5 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
-import i18nConfig from '../i18n.json'
-
-function documentLang({ __NEXT_DATA__ }) {
-  const { page } = __NEXT_DATA__
-  const [, langQuery] = page.split('/')
-  const lang = i18nConfig.allLanguages.find((l) => l === langQuery)
-
-  return lang || i18nConfig.defaultLanguage
-}
+import documentLang from 'next-translate/documentLang'
 
 export default class MyDocument extends Document {
   render() {
