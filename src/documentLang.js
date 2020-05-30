3584718de9c8ab12cb169cc37b5d4181c43aeaf3
Aral Roca Gomez <aral-rg@hotmail.com>
2020-05-31 00:09:22 +0200

    Fix documentLang in TypeScript

--- src/documentLang.js
+++ src/documentLang.js
@@ -1,6 +1,9 @@
+const path = require('path')
+const fs = require('fs')
+
 export default function documentLang({ __NEXT_DATA__ }) {
-  const { resolve } = require('path')
-  const i = require(resolve(process.cwd() + '/i18n.json'))
+  const configDir = path.join(process.cwd(), 'i18n.json')
+  const i = JSON.parse(fs.readFileSync(configDir))
   const { page } = __NEXT_DATA__
   const [, langQuery] = page.split('/')
   const lang = i.allLanguages.find((l) => l === langQuery)
