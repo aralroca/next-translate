c3c13c5a3e5e24d4bdc70dd57d39f27b8384bbdb
Aral Roca Gomez <aral-rg@hotmail.com>
2020-09-21 13:43:41 +0200

    Fix lang for custom server on fixHref helper

--- src/fixHref.js
+++ src/fixHref.js
@@ -4,5 +4,9 @@ import appendLangPrefix from './_helpers/appendLangPrefix'
 export default (href, lng) => {
   const isRoot =
     i.defaultLangRedirect !== 'lang-path' && i.defaultLanguage === lng
-  return i.isStaticMode && !isRoot ? appendLangPrefix(href, lng) : href
+  const url = i.isStaticMode && !isRoot ? appendLangPrefix(href, lng) : href
+
+  if (i.isStaticMode) return url
+
+  return url.includes('?') ? `${url}&lang=${lng}` : `${url}?lang=${lng}`
 }
