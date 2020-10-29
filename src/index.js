24da1bcbb5fa4bcd8aa2c3f31f8b1a59433671ee
Aral Roca Gomez <aral-rg@hotmail.com>
2020-10-29 19:21:15 +0100

    Add warning when using global imports

--- src/index.js
+++ src/index.js
@@ -5,3 +5,7 @@ export { default as appWithI18n } from './appWithI18n'
 export { default as i18nMiddleware } from './i18nMiddleware'
 export { default as useTranslation } from './useTranslation'
 export { default as withTranslation } from './withTranslation'
+
+console.warn(
+  "🚨 [next-translate] Replace all global imports like `import {useTranslate} from 'next-translate'` to specific imports like `import useTranslate from 'next-translate/useTranslate'`. Globals imports will be removed in next releases."
+)
