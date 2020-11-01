c5c8008f4f6c11bde76a4cb218a2bbc3eb9e8fd7
crouton <68943932+croutonn@users.noreply.github.com>
2020-11-02 02:59:00 +0900

    Fix builder problem with group exports (#298)

--- cli/builder.js
+++ cli/builder.js
@@ -214,8 +214,13 @@ ${exports}
 }
 
 function hasExportName(data, name) {
-  return data.match(
-    new RegExp(`export (const|var|let|async function|function) ${name}`)
+  return (
+    data.match(
+      new RegExp(`export (const|var|let|async function|function) ${name}`)
+    ) ||
+    data.match(
+      new RegExp(`export\\s*\\{[^}]*(?<!\\w)${name}(?!\\w)[^}]*\\}`, 'm')
+    )
   )
 }
 
