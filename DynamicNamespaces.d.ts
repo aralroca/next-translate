9db8b7373a56e5df599d14db55a602fea09f6668
croutonn <croutonna@gmail.com>
2020-11-02 17:09:21 +0900

    Add typescript declaration files

---
+++ DynamicNamespaces.d.ts
@@ -0,0 +1,4 @@
+import * as React from 'react'
+import { DynamicNamespacesProps } from './'
+declare const DynamicNamespaces: React.ComponentType<DynamicNamespacesProps>
+export default DynamicNamespaces
---
+++ Trans.d.ts
@@ -0,0 +1,4 @@
+import * as React from 'react'
+import { TransProps } from './'
+declare const Trans: React.ComponentType<TransProps>
+export default Trans
