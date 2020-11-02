72ebacdc41e104ec82b909328b72d5f843d3b73b
Aral Roca Gomez <aral-rg@hotmail.com>
2020-11-02 17:58:25 +0100

    Remove builder tests right now

--- examples/with-build-step/i18n.js
+++ examples/with-build-step/i18n.js
@@ -1,9 +1,9 @@
 module.exports = {
   locales: ['en', 'ca', 'es'],
   defaultLocale: 'en',
-  loadLocaleFrom: (locale, ns) =>
-    import(`./locales/${locale}/${ns}.json`).then((m) => m.default),
-  loader: true,
+  // loadLocaleFrom: (locale, ns) =>
+  //   import(`./locales/${locale}/${ns}.json`).then((m) => m.default),
+  // loader: true,
   pages: {
     '*': ['common'],
     '/404': ['error'],
--- src/_loader/_appTransformation.js
+++ src/_loader/_appTransformation.js
@@ -1,11 +1,16 @@
 function _appTransformation(code, { i18nFile, arePagesInsideSrc }) {
-  const configPath = arePagesInsideSrc ? `../..${i18nFile}` : `..${i18nFile}`
+  const prefix = arePagesInsideSrc ? '../..' : '..'
+  const configPath = `${prefix}${i18nFile}`
+  const defaultLoadLocaleFrom = `${prefix}/locales/\${l}/\${n}.json`
 
   return `
     import __i18nConfig from '${configPath}'
     import __appWithI18n from 'next-translate/appWithI18n'
     ${code.replace('export default', 'const __Page_Next_Translate__ =')}
-    export default __appWithI18n(__Page_Next_Translate__, __i18nConfig);
+    export default __appWithI18n(__Page_Next_Translate__, {
+      loadLocaleFrom: (l, n) => import('${defaultLoadLocaleFrom}').then(m => m.default),
+      ...__i18nConfig
+    });
   `
 }
 
