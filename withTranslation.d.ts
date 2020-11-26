621a6f0df2e1a64c28ed731b06cbd258f8a6cd9e
Aral Roca Gomez <aral-rg@hotmail.com>
2020-11-26 18:26:46 +0100

    Fix withTranslation type (#354)

--- withTranslation.d.ts
+++ withTranslation.d.ts
@@ -1,6 +1,5 @@
 import * as React from 'react'
-import { I18n } from './'
 declare function withTranslation<P = unknown>(
   component: React.ComponentType<P>
-): React.ComponentType<P & { i18n: I18n }>
+): React.ComponentType<Omit<P, 'i18n'>>
 export default withTranslation
