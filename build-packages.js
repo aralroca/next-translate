faf555a3b2de576c3bb86114855863eecab1708f
Aral Roca Gomez <aral-rg@hotmail.com>
2020-12-04 12:03:48 +0100

    Clean build plugin packages

--- build-packages.js
+++ build-packages.js
@@ -2,34 +2,36 @@ const path = require('path')
 const fs = require('fs')
 
 const supportedExt = new Set(['js', 'ts', 'tsx'])
-const src = path.join(__dirname, 'src')
-const plugin = path.join(__dirname, 'src/plugin')
 
-function createPackages(folder, prefix, subfolder = '') {
+function createPackageFromFile(file, prefix, subfolder) {
+  const [name, ext] = file.split('.')
+
+  if (!supportedExt.has(ext)) return
+
+  fs.mkdirSync(`${subfolder}${name}`)
+
+  const packageJSON = JSON.stringify(
+    {
+      internal: true,
+      main: `${prefix}lib/cjs/${subfolder}${name}.js`,
+      module: `${prefix}lib/esm/${subfolder}${name}.js`,
+      types: `${prefix}lib/esm/${subfolder}${name}.d.ts`,
+    },
+    undefined,
+    2
+  )
+
+  fs.writeFileSync(`${subfolder}${name}/package.json`, packageJSON)
+}
+
+function createPackagesFromFolder(folder, prefix, subfolder = '') {
   const files = fs.readdirSync(folder)
 
   files.forEach((file) => {
-    const [name, ext] = file.split('.')
-
-    if (!supportedExt.has(ext)) return
-
-    fs.mkdirSync(`${subfolder}${name}`)
-
-    const packageJSON = JSON.stringify(
-      {
-        internal: true,
-        main: `${prefix}lib/cjs/${subfolder}${name}.js`,
-        module: `${prefix}lib/esm/${subfolder}${name}.js`,
-        types: `${prefix}lib/esm/${subfolder}${name}.d.ts`,
-      },
-      undefined,
-      2
-    )
-
-    fs.writeFileSync(`${subfolder}${name}/package.json`, packageJSON)
+    createPackageFromFile(file, prefix, subfolder)
   })
 }
 
-createPackages(src, '../')
+createPackagesFromFolder(path.join(__dirname, 'src'), '../')
 fs.mkdirSync('plugin')
-createPackages(plugin, '../../', 'plugin/')
+createPackageFromFile('loader.tsx', '../../', 'plugin/')
