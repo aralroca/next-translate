af935cea9cbb2675488924904eb4ce92850104bf
Andre Hsu <4470828+andrehsu@users.noreply.github.com>
2021-01-25 21:15:47 +0800

    Fix default namespace not working for fallback keys

--- src/wrapTWithDefaultNs.tsx
+++ src/wrapTWithDefaultNs.tsx
@@ -7,6 +7,19 @@ export default function wrapTWithDefaultNs(oldT: any, defaultNs?: string) {
   function t(key = '', query, options): Translate {
     let k = Array.isArray(key) ? key[0] : key
     if (!k.includes(':')) k = `${defaultNs}:${k}`
+
+    // Use default namespace for query.fallback keys
+    if (query?.fallback) {
+      if (Array.isArray(query.fallback)) {
+        query.fallback = query.fallback.map((k) =>
+          k.includes(':') ? k : `${defaultNs}:${k}`
+        )
+      } else {
+        const k = query.fallback
+        query.fallback = k.includes(':') ? k : `${defaultNs}:${k}`
+      }
+    }
+
     return oldT(k, query, options)
   }
 
