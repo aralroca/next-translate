a76e2a77b2974785e77569bffc29d4f4b03968b9
Aral Roca Gomez <aral-rg@hotmail.com>
2021-05-01 15:14:47 +0200

    Revert "Add useMemo to useTranslation"
    This reverts commit 8abc45855cdc95bed873657379499c758483829a.
    
    # Conflicts:
    #	package.json

--- src/useTranslation.tsx
+++ src/useTranslation.tsx
@@ -1,4 +1,4 @@
-import { useContext, useMemo } from 'react'
+import { useContext } from 'react'
 import { I18n } from '.'
 import wrapTWithDefaultNs from './wrapTWithDefaultNs'
 import I18nContext from './_context'
@@ -7,9 +7,6 @@ export default function useTranslation(defaultNs?: string): I18n {
   const ctx = useContext(I18nContext)
   return {
     ...ctx,
-    t: useMemo(() => wrapTWithDefaultNs(ctx.t, defaultNs), [
-      ctx.lang,
-      defaultNs,
-    ]),
+    t: wrapTWithDefaultNs(ctx.t, defaultNs),
   }
 }
