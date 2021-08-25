5eac0b09e04a1e3c805da574f78df4f33e30f1e8
Aral Roca Gomez <aral-rg@hotmail.com>
2021-08-25 18:41:04 +0200

    Update deps (#673)

--- src/getConfig.tsx
+++ src/getConfig.tsx
@@ -2,6 +2,6 @@ import { LoaderConfig } from './index'
 
 export default function getConfig(): LoaderConfig {
   // We are not using globalThis to support Node 10
-  const g = typeof window === 'undefined' ? global : window
+  const g: any = typeof window === 'undefined' ? global : window
   return g.i18nConfig
 }
