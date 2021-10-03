e35ec76db90666926891e7b3a99c014039715f1d
Aral Roca Gomez <aral-rg@hotmail.com>
2021-10-03 11:58:17 +0200

    Add app and integrations folders for Blitz

--- src/plugin/index.ts
+++ src/plugin/index.ts
@@ -10,7 +10,28 @@ export default function nextTranslate(nextConfig: any = {}) {
     path.relative(pkgDir(), process.env.NEXT_TRANSLATE_PATH || '.')
   )
 
-  const arePagesInsideSrc = fs.existsSync(path.join(dir, 'src/pages'))
+  let hasGetInitialPropsOnAppJs = false
+  let pagesInDir = 'pages'
+
+  // https://github.com/blitz-js/blitz/blob/canary/nextjs/packages/next/build/utils.ts#L54-L59
+  if (fs.existsSync(path.join(dir, 'src/pages'))) {
+    pagesInDir = 'src/pages'
+  } else if (fs.existsSync(path.join(dir, 'app/pages'))) {
+    pagesInDir = 'app/pages'
+  } else if (fs.existsSync(path.join(dir, 'integrations/pages'))) {
+    pagesInDir = 'integrations/pages'
+  }
+
+  const pagesPath = path.join(dir, pagesInDir)
+  const app = fs
+    .readdirSync(pagesPath)
+    .find((page: string) => page.startsWith('_app.'))
+
+  if (app) {
+    const code = fs.readFileSync(path.join(pagesPath, app)).toString('UTF-8')
+    hasGetInitialPropsOnAppJs =
+      !!code.match(/\WgetInitialProps\W/g) || hasHOC(code)
+  }
 
   const i18n = nextConfig.i18n || {}
   const {
@@ -21,19 +42,6 @@ export default function nextTranslate(nextConfig: any = {}) {
     logger,
     ...restI18n
   } = require(path.join(dir, 'i18n'))
-
-  // Check if exist a getInitialProps on _app.js
-  let hasGetInitialPropsOnAppJs = false
-  const pagesPath = path.join(dir, arePagesInsideSrc ? '/src/pages' : '/pages')
-  const app = fs
-    .readdirSync(pagesPath)
-    .find((page: string) => page.startsWith('_app.'))
-
-  if (app) {
-    const code = fs.readFileSync(path.join(pagesPath, app)).toString('UTF-8')
-    hasGetInitialPropsOnAppJs =
-      !!code.match(/\WgetInitialProps\W/g) || hasHOC(code)
-  }
 
   return {
     ...nextConfig,
