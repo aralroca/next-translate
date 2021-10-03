38f386f25d850b8c4bc0e1d9c1a41d11eba47494
Aral Roca Gomez <aral-rg@hotmail.com>
2021-10-03 12:16:21 +0200

    Add pagesInPath configuration property

--- src/plugin/index.ts
+++ src/plugin/index.ts
@@ -10,16 +10,29 @@ export default function nextTranslate(nextConfig: any = {}) {
     path.relative(pkgDir(), process.env.NEXT_TRANSLATE_PATH || '.')
   )
 
+  const i18n = nextConfig.i18n || {}
+  let {
+    locales,
+    defaultLocale,
+    loader = true,
+    pagesInDir,
+    pages,
+    logger,
+    ...restI18n
+  } = require(path.join(dir, 'i18n'))
+
   let hasGetInitialPropsOnAppJs = false
-  let pagesInDir = 'pages'
 
   // https://github.com/blitz-js/blitz/blob/canary/nextjs/packages/next/build/utils.ts#L54-L59
-  if (fs.existsSync(path.join(dir, 'src/pages'))) {
-    pagesInDir = 'src/pages'
-  } else if (fs.existsSync(path.join(dir, 'app/pages'))) {
-    pagesInDir = 'app/pages'
-  } else if (fs.existsSync(path.join(dir, 'integrations/pages'))) {
-    pagesInDir = 'integrations/pages'
+  if (!pagesInDir) {
+    pagesInDir === 'pages'
+    if (fs.existsSync(path.join(dir, 'src/pages'))) {
+      pagesInDir = 'src/pages'
+    } else if (fs.existsSync(path.join(dir, 'app/pages'))) {
+      pagesInDir = 'app/pages'
+    } else if (fs.existsSync(path.join(dir, 'integrations/pages'))) {
+      pagesInDir = 'integrations/pages'
+    }
   }
 
   const pagesPath = path.join(dir, pagesInDir)
@@ -32,16 +45,6 @@ export default function nextTranslate(nextConfig: any = {}) {
     hasGetInitialPropsOnAppJs =
       !!code.match(/\WgetInitialProps\W/g) || hasHOC(code)
   }
-
-  const i18n = nextConfig.i18n || {}
-  const {
-    locales,
-    defaultLocale,
-    loader = true,
-    pages,
-    logger,
-    ...restI18n
-  } = require(path.join(dir, 'i18n'))
 
   return {
     ...nextConfig,
