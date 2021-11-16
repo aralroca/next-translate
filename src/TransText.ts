d0d08adf1fa63debc94d2b8114cb48ba9ae96629
dndhm <dneedham@gmail.com>
2021-11-16 19:54:54 +0000

    Update TransText with more sensible type

--- src/TransText.ts
+++ src/TransText.ts
@@ -1,17 +1,18 @@
-import { useMemo } from 'react';
+import { useMemo } from 'react'
 
-import formatElements from './formatElements';
-import { TransProps } from '.';
+import formatElements from './formatElements'
+import { TransProps } from '.'
 
-type ValueTransProps = Omit<TransProps, 'i18nKey'> & {
-  text: string;
+type ValueTransProps = Pick<TransProps, 'components'> & {
+  text: string
 }
 
-export default function TransText({
-  text,
-  components,
-}: ValueTransProps): any {
-  return useMemo(() => (!components || components.length === 0)
-    ? text
-    : formatElements(text, components), [text, components]) as string;
+export default function TransText({ text, components }: ValueTransProps): any {
+  return useMemo(
+    () =>
+      !components || components.length === 0
+        ? text
+        : formatElements(text, components),
+    [text, components]
+  ) as string
 }
