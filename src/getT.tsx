7ccf8bb9fb618a7880efbcd70d20ec10cf681103
mustapha <mustapha@zadgroup.net>
2022-06-11 12:03:37 +0100

    feat(getT): #861 get multiple namespaces

--- src/getT.tsx
+++ src/getT.tsx
@@ -3,16 +3,29 @@ import transCore from './transCore'
 import wrapTWithDefaultNs from './wrapTWithDefaultNs'
 import { I18nDictionary, LocaleLoader } from './index'
 
-export default async function getT(locale = '', namespace = '') {
+export default async function getT(
+  locale = '',
+  namespace: string | string[] = ''
+) {
   const config = getConfig()
   const defaultLoader = async () => Promise.resolve<I18nDictionary>({})
   const lang = locale || config.defaultLocale || ''
   const loader: LocaleLoader = config.loadLocaleFrom || defaultLoader
-  const allNamespaces = { [namespace]: await loader(lang, namespace) }
+
+  const namespaces = Array.isArray(namespace) ? namespace : [namespace]
+
+  const allNamespaces: Record<string, I18nDictionary> = {}
+  await Promise.all(
+    namespaces.map(async (namespace) => {
+      allNamespaces[namespace] = await loader(lang, namespace)
+    })
+  )
+
   const localesToIgnore = config.localesToIgnore || ['default']
   const ignoreLang = localesToIgnore.includes(lang)
   const pluralRules = new Intl.PluralRules(ignoreLang ? undefined : lang)
   const t = transCore({ config, allNamespaces, pluralRules, lang })
 
-  return wrapTWithDefaultNs(t, namespace)
+  const defaultNamespace = namespaces[0]
+  return wrapTWithDefaultNs(t, defaultNamespace)
 }
