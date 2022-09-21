bdf48d6696663c2b96ac7b4ae9d1aa8fcfe227bb
Mikołaj Koropecki <mkbctrl@gmail.com>
2022-09-21 17:38:00 +0200

    Add string[] to I18nDictionary interface
    fixes: https://github.com/aralroca/next-translate/issues/925

--- src/index.tsx
+++ src/index.tsx
@@ -89,7 +89,7 @@ export interface I18nLogger {
 }
 
 export interface I18nDictionary {
-  [key: string]: string | I18nDictionary
+  [key: string]: string | string[] | I18nDictionary
 }
 
 export interface DynamicNamespacesProps {
