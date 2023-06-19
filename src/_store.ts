1a4ad01d6a16927ccd0f73b12c3efd8da6d28e8d
Aral Roca Gomez <aral-rg@hotmail.com>
2023-06-19 21:27:53 +0200

    Update package.json version (#1075)

---
+++ src/_store.ts
@@ -0,0 +1,18 @@
+import { DataForStoreType } from '.'
+
+let data: DataForStoreType
+
+export default {
+  set(storeData: DataForStoreType) {
+    data = {
+      ...storeData,
+      namespaces: {
+        ...(data?.namespaces || {}),
+        ...(storeData.namespaces || {}),
+      },
+    }
+  },
+  get(): DataForStoreType {
+    return data
+  },
+}
