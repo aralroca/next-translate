65bb290bf08076f5eb014e6e45e6fe04dad96d79
Tim O. Peters <timotewpeters@gmail.com>
2023-11-04 13:20:00 +0100

    fix: ignore empty lang value #1162 (#1163)

--- src/createTranslation.tsx
+++ src/createTranslation.tsx
@@ -5,7 +5,7 @@ import wrapTWithDefaultNs from './wrapTWithDefaultNs'
 export default function createTranslation(defaultNS?: string) {
   const { lang, namespaces, config } = globalThis.__NEXT_TRANSLATE__ ?? {}
   const localesToIgnore = config.localesToIgnore || ['default']
-  const ignoreLang = localesToIgnore.includes(lang)
+  const ignoreLang = !lang || localesToIgnore.includes(lang)
   const t = transCore({
     config,
     allNamespaces: namespaces,
