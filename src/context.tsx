cad6e4dd9c95b71344479daf1923b31512d2c8a2
Aral Roca <aral-rg@hotmail.com>
2024-01-22 16:17:53 +0100

    fix: fix useContext error + update example

--- src/context.tsx
+++ src/context.tsx
@@ -1,13 +1,13 @@
-import { createContext } from 'react'
 import { I18n } from '.'
+import React from 'react'
 
 let context
 
 // For serverComponents (app-dir), the context cannot be used and
 // this makes that all the imports to here don't break the build.
 // The use of this context is inside each util, depending pages-dir or app-dir.
-if (typeof createContext === 'function') {
-  context = createContext<I18n>({
+if (typeof React.createContext === 'function') {
+  context = React.createContext<I18n>({
     t: (k) => (Array.isArray(k) ? k[0] : k),
     lang: '',
   })
