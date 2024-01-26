17df895f9767de961f851439a6efdf67d31eb8f3
Hichem Fantar <fantar.hichem@gmail.com>
2024-01-26 01:12:03 +0100

    Add type safety for getT function (#1184)
    * Update type-safety.md
    
    * Update next-translate.d.ts with new TranslateFunction interface
    
    * Refactor translation types in next-translate.d.ts
    
    * Refactor type safety in TranslateFunction

--- examples/with-app-directory/next-translate.d.ts
+++ examples/with-app-directory/next-translate.d.ts
@@ -1,23 +1,37 @@
 import type { Paths, I18n, Translate } from 'next-translate'
 
+type Tail<T> = T extends [unknown, ...infer Rest] ? Rest : never;
+
 export interface TranslationsKeys {
+  // Example with "common" and "home" namespaces in "en" (the default language):
   common: Paths<typeof import('./locales/en/common.json')>
   home: Paths<typeof import('./locales/en/home.json')>
+  // Specify here all the namespaces you have...
 }
 
-export interface TypeSafeTranslate<Namespace extends keyof TranslationsKeys>
+type TranslationNamespace = keyof TranslationsKeys;
+
+export interface TranslateFunction<Namespace extends TranslationNamespace>  {
+  (
+    key: TranslationsKeys[Namespace],
+    ...rest: Tail<Parameters<Translate>>
+  ): string
+  <T extends string>(template: TemplateStringsArray): string
+};
+
+export interface TypeSafeTranslate<Namespace extends TranslationNamespace>
   extends Omit<I18n, 't'> {
-  t: {
-    (
-      key: TranslationsKeys[Namespace],
-      ...rest: Tail<Parameters<Translate>>
-    ): string
-    <T extends string>(template: TemplateStringsArray): string
-  }
+  t: TranslateFunction<Namespace>
 }
 
 declare module 'next-translate/useTranslation' {
   export default function useTranslation<
-    Namespace extends keyof TranslationsKeys
+    Namespace extends TranslationNamespace
   >(namespace: Namespace): TypeSafeTranslate<Namespace>
+}
+
+declare module 'next-translate/getT' {
+  export default function getT<
+    Namespace extends TranslationNamespace
+  >(locale?: string, namespace: Namespace): Promise<TranslateFunction<Namespace>>  
 }
