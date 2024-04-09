4085539ca3d08cac0d4a6ee60f79df879a9da14e
Jesse Martin <1626240+jessemartin@users.noreply.github.com>
2024-04-09 06:17:22 -0700

    fix: stabilize t in app directory (#1206)
    Co-authored-by: Jesse <Jesse>

--- src/createTranslation.tsx
+++ src/createTranslation.tsx
@@ -1,3 +1,5 @@
+import { useMemo } from 'react'
+import isServer from './isServer'
 import transCore from './transCore'
 import wrapTWithDefaultNs from './wrapTWithDefaultNs'
 
@@ -6,12 +8,17 @@ export default function createTranslation(defaultNS?: string) {
   const { lang, namespaces, config } = globalThis.__NEXT_TRANSLATE__ ?? {}
   const localesToIgnore = config.localesToIgnore || ['default']
   const ignoreLang = !lang || localesToIgnore.includes(lang)
-  const t = transCore({
-    config,
-    allNamespaces: namespaces,
-    pluralRules: new Intl.PluralRules(ignoreLang ? undefined : lang),
-    lang,
-  })
+  const getT = () => {
+    const t = transCore({
+      config,
+      allNamespaces: namespaces,
+      pluralRules: new Intl.PluralRules(ignoreLang ? undefined : lang),
+      lang,
+    })
 
-  return { t: wrapTWithDefaultNs(t, defaultNS), lang }
+    return wrapTWithDefaultNs(t, defaultNS)
+  }
+
+  const t = isServer() ? getT() : useMemo(getT, [defaultNS])
+  return { t, lang }
 }
